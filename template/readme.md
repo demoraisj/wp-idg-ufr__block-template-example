737776cd9ef22725f54e3e5876d1b4ad544f52df
juniorjmm <josemjunior17@gmail.com>
2021-10-20 15:33:56 -0400

    init

---
+++ template/readme.md
@@ -0,0 +1,28 @@
+# wp-idg-ufr__block-avatar
+
+> Bloco de $block-title-capitalized$ baseado no componente do Design System do Governo Federal <br />
+> @see https://dsgov.estaleiro.serpro.gov.br/components/$block-name$
+
+```
+=== $block-name$ ===
+Tags:              block
+Tested up to:      5.8.0
+Stable tag:        0.1.0
+```
+
+## Instalação
+
+Trata-se se um plugin wordpress, copie o conteúdo para wp-contet/plugins e compile.
+```bash
+npm i && npm run build
+```
+
+## Desenvolvimento
+
+```bash
+# Atualiza os componentes "wp-idg-ufr__block-components"
+# @see https://github.com/juniorjmm/wp-idg-ufr__block-components
+npm run components-update
+```
+Demais comandos são herdados do ambiente de desevolvimento @wordpress/create-block
+
---
+++ template/src/edit.js
@@ -0,0 +1,105 @@
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { UFRBlockHeader, UFRSelect } from 'wp-idg-ufr__block-components';
+import { Fragment } from 'react';
+
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/developers/block-api/block-edit-save/#edit
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function edit( { attributes, setAttributes, isSelected } ) {
+	/**
+	 * Desestruturação dos atributos do bloco registrados em block.json -> "attributes"
+	 */
+	const { position } = attributes;
+
+	/**
+	 * Opções para configuração de posição do botão
+	 *
+	 * @type { {label: string, value: string}[] }
+	 */
+	const positioningOptions = [
+		{
+			label: 'Esquerda',
+			value: 'start',
+		},
+		{
+			label: 'Centro',
+			value: 'center',
+		},
+		{
+			label: 'Direita',
+			value: 'end',
+		},
+	];
+
+	/**
+	 * Renderiza o conteúdo. Esconde as configurações do bloco quando ele não está selecionado.
+	 *
+	 * @param { boolean } selected
+	 * @return {JSX.Element} Elemento principal condicional
+	 */
+	function ConditionalMainContentRender( selected ) {
+		return selected ? (
+			// Visuzalização quando selecionado
+			<div
+				{ ...useBlockProps( {
+					className: 'edit block-responsive ufr-block-component',
+				} ) }
+			>
+				<div className="row align-items-center">
+					<div className="col config">
+						<UFRBlockHeader
+							title="$block-title-capitalized$"
+							subtitle="Configure a aparenência do $block-title-lowcase$ abaixo. Outras configurações podem estar disponíveis no menu á direita."
+						/>
+					</div>
+
+					<div className="row preview">
+						{ /* Renderize o componente aqui */ }
+					</div>
+				</div>
+			</div>
+		) : (
+			// Visuzalização quando não selecionado
+			<div
+				{ ...useBlockProps( {
+					className: 'show block-responsive ufr-block-component',
+				} ) }
+			>
+				<div className="row" >
+					<div
+						className={ `col-12 d-flex justify-content-${ position }` }
+					>
+						{ /* Renderize o componente aqui */ }
+					</div>
+				</div>
+			</div>
+		);
+	}
+
+	return (
+		<Fragment>
+			<InspectorControls key="setting">
+				<div id="ufrControls">
+					<fieldset>
+						<UFRSelect
+							label="Posição Horizontal do $block-title-capitalized$"
+							options={ positioningOptions }
+							value={ position }
+							attr="position"
+							setter={ setAttributes }
+						/>
+					</fieldset>
+				</div>
+			</InspectorControls>
+
+			{ ConditionalMainContentRender( isSelected ) }
+		</Fragment>
+	);
+}
