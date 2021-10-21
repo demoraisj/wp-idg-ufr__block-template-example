6ec79dda5f88bc291b7b86b4abd4ab7bb84bbde6
juniorjmm <josemjunior17@gmail.com>
2021-10-21 15:35:09 -0400

    bugfix

--- wp-idg-ufr__block-$block-name$/src/index.js
+++ wp-idg-ufr__block-$block-name$/src/index.js
@@ -25,7 +25,7 @@ import save from './save';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
  */
-registerBlockType('create-block/ufr-card', {
+registerBlockType('create-block/ufr-$block-name$', {
 	/**
 	 * @see ./edit.js
 	 */
