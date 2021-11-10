2f886d62c5ffefa5be5a692e211266e96100aa41
juniorjmm <josemjunior17@gmail.com>
2021-11-10 16:25:46 -0400

    add plugin url to js globals

--- wp-idg-ufr__block-$block-name$/wp-idg-ufr__block-$block-name$.php
+++ wp-idg-ufr__block-$block-name$/wp-idg-ufr__block-$block-name$.php
@@ -21,6 +21,10 @@
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
 function create_block_wp_idg_ufr_block_$block-name$_block_init() {
+	global $blocks_url;
+
+    $blocks_url = array_merge($block_url ?? [], ['$block-name$' => plugin_dir_url(__FILE__)]);
+
 	register_block_type( __DIR__ );
 }
 add_action( 'init', 'create_block_wp_idg_ufr_block_$block-name$_block_init' );
